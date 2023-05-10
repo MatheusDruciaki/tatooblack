ceecc56f14c94f9d36e2cc97c3d6c77d36cdffac
MatheusDruciaki <133068490+MatheusDruciaki@users.noreply.github.com>
2023-05-10 08:13:32 -0300

    Create rodape.inc.php

---
+++ rodape.inc.php
@@ -0,0 +1,15 @@
+<body>
+    <footer>@tatooblack
+</footer>
+<ul class="social">
+    <li><a href="https://www.facebook.com/Adoroexclusividadesartesanais">Facebook</a></li>
+    <li><a href="http://twitter.com">Twitter</a></li>
+    <li><a href="https://www.instagram.com/">instagram</a></li>
+
+</ul>
+    
+    
+   
+    
+</body>
+</html>
