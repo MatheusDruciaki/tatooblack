96bda95ffb19d1e4ac1b6266944dc6de36957cc0
MatheusDruciaki <133068490+MatheusDruciaki@users.noreply.github.com>
2023-05-10 08:10:05 -0300

    Create sobre.php

---
+++ sobre.php
@@ -0,0 +1,52 @@
+<?php include 'inc/cabecalho.inc.php';?> 
+    <div class="corpo">
+    <h1 class="title"> <mark class="estilo_tb"> &#128123TatooBlack </mark></h1>
+   
+    <p> A Empresa <strong>Tattoblack</strong>  é uma empresa especializada em tatuar as mais variadas e bonitas tattos nas mais variadas partes do corpo humano, fazemos <i><mark class="estilo_tato">tatoo's</mark></i>
+        coloridas, de estilos personalizados e tatuamos qualquer imagem. </p> 
+
+        <hr>
+
+        <figure>
+            <img src="img/studio.jpg" alt="studio" width="50%">
+            <figcaption> <mark class="estilo_st"> Studio Tatooblack</mark></figcaption>
+        </figure>
+
+        <p> &#9757 Situada na cidade de <b> <small>Ponta Grossa </small>  </b> no estado do <b> <small>Paraná</small></b>, dispõe de <sub>8</sub> salas de tatoos com <sup>16</sup><em>profissionais</em> para realizar as mais belas artes. Nosso material é de primeira <mark class="estilo_q">qualidade</mark> e garantimos a qualidade da tatuagem por mais de <del>10 anos</del> <ins>12 anos</ins> sem necessidade de retoques.
+        </p>
+
+        <hr><br>
+
+        <figure class="figtato">
+            <img class="realista" src="img/tatuagens.jpg" alt="tatoos">
+            <figcaption class="tato_real"> <mark class="estilo_trc"> Tatuagem Realista/colorida!</mark> </figcaption>
+        </figure>
+        
+        <p>
+            &#9757 Nossas tatuagens coloridas nos dão uma impressão  realista do desenho tatuado, por este motivo nos leva a reviver o acontecimento retratado na imagem.
+        </p>
+        
+        <hr>
+
+        <h2 class="tatuadores"> <mark class="estilo_t">Tatuadores</mark></h2>
+        <p class="prt"> 
+           <mark class="estilo_prt">Principais Tatuadores:</mark>
+           <ul class="lista">
+            <li>João Quebra mão</li>
+            <li>Caio Cicatriz </li>
+            <li>Paulinho loko</li>
+            <li>Bruna Paulada</li>    
+            <li>Tokoku nakama</li>
+            <li>Leticia amavaanél</li>
+            <li>Cauã xupetinha</li>
+           </ul>
+
+        </p>
+        <h2 class="crt"> <mark class="estilo_crt">Características das nossas tatuagens</mark></h2>
+        <ol class="lista2">
+            <li> Maior durabilidade </li> 
+            <li>Cores mais realistas </li>
+            <li>Fazemos qualquer tipo de tatuagem</li>
+            <li>Traçado perfeito!</li>
+        </ol>
+        <?php include 'inc/rodape.inc.php';?>
