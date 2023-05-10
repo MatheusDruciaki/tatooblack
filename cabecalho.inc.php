7d6e00587c58a7b3d21d117bb4f12603a6c9f205
MatheusDruciaki <133068490+MatheusDruciaki@users.noreply.github.com>
2023-05-10 08:14:29 -0300

    Create cabecalho.inc.php

---
+++ cabecalho.inc.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=10">
+    <title>TatooBlack</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+  
+    <header class="sombra">
+     <a href="index.php"><img src="img/LOGO.png" alt="logo Tatooblack" width="30%"></a>
+  <!-- INICIO "MECANISMO DE BUSCA"-->
+    <session class="busca">
+        <h2>Pesquisar:</h2>
+            <form>
+                <input type="search">
+                <button>Buscar</button>
+            </form>
+    </session>
+          <!-- FIM "MECANISMO" -->
+            <!--INICIO "MENU"-->
+        <nav class="menu_opcoes">
+                <ul>
+                    <li><a href="#">Sua Conta </a></li>
+                    <li><a href="#">Fotos </a></li>
+                    <li><a href="#">Cartão Fidelidade</a></li>
+                    <li><a href="#">Ajuda</a></li> 
+                    <li><a href="sobre.php">Sobre</a></li>
+                </ul>
+        </nav>
+            <!-- FIM "MENU" -->
+    </header>
