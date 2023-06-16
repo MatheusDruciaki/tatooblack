82f9ec3df4d831d283ea14ffa0bd436bd39e4eec
Matheus <133068490+MatheusDruciaki@users.noreply.github.com>
2023-06-16 00:38:20 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php include 'inc/cabecalho.inc.php' ;?>
+      <h1>cabeeçalho
+    
+      </h1>
+    <br><hr>
+    <!--<footer>
+        <h1 style="color: green; background-color: black;">AQUI SERÁ O RODAPÉ DO SITE!!</h1>
+        <br><hr>-->
+        <?php include 'inc/rodape.inc.php' ;?>
+   
